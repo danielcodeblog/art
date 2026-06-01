8ce3e4f4990186766ca89870d6212dc60f09d2bc
danielcodeblog <114114540+danielcodeblog@users.noreply.github.com>
2026-06-01 13:03:23 +0100

    Update Works.tsx

--- components/Works.tsx
+++ components/Works.tsx
@@ -9,7 +9,7 @@ const projects = [
     id: 1,
     title: 'SILENT ECHOES',
     category: 'NARRATIVE',
-    video: 'https://player.vimeo.com/video/1175901213?autoplay=0&loop=1&badge=0&autopause=0&api=1&muted=1&controls=0',
+    video: 'https://vimeo.com/1197342487?share=copy&fl=sv&fe=ci?autoplay=0&loop=1&badge=0&autopause=0&api=1&muted=1&controls=0',
     poster: 'https://picsum.photos/seed/silent-echoes/1920/1080',
   },
   {
@@ -42,9 +42,9 @@ const projects = [
   },
   {
     id: 6,
-    title: 'DINOVATE',
+    title: 'STARS',
     category: 'CLASSIC',
-    video: 'https://player.vimeo.com/video/1185612995?autoplay=0&loop=1&badge=0&autopause=0&api=1&muted=1&controls=0',
+    video: 'https://vimeo.com/1197347649?autoplay=0&loop=1&badge=0&autopause=0&api=1&muted=1&controls=0',
     poster: 'https://picsum.photos/seed/p2/800/1000',
   },
 ];
